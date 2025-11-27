486d9742e4d8544f0235ab080f13a59b931dfd3b
Juan Ruben Nolasco Silva <juan.ruben.nolasco.silva@gmail.com>
2025-11-27 17:46:25 -0600

    CorsConfig and adding uuid id to QueryResponse

--- src/main/java/com/example/ai/synthetic_data_generator_ai/controller/TalkToYourDataController.java
+++ src/main/java/com/example/ai/synthetic_data_generator_ai/controller/TalkToYourDataController.java
@@ -2,7 +2,10 @@ package com.example.ai.synthetic_data_generator_ai.controller;
 
 import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,21 +17,24 @@ import com.example.ai.synthetic_data_generator_ai.dto.QueryResponse;
 import com.example.ai.synthetic_data_generator_ai.llm.LLMSchemaAssistantClient;
 import com.example.ai.synthetic_data_generator_ai.service.DynamicDataService;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
+@CrossOrigin()
 @RestController
+@Valid
 @RequiredArgsConstructor
-@RequestMapping("/api/v1/query")
+@RequestMapping("/api/v1/schema-assistant")
 public class TalkToYourDataController {
 
     private static final String I_CANNOT_ANSWER_THIS_QUESTION_WITH_THE_AVAILABLE_DATA = "I cannot answer this question with the available data.";
     private final DynamicDataService dynamicDataService;
     private final LLMSchemaAssistantClient llmClient;
 
-    @PostMapping
-    public QueryResponse query(@RequestBody QueryRequest request) {
+    @PostMapping("questions")
+    public ResponseEntity<QueryResponse> query(@RequestBody QueryRequest request) {
 
         LearnDatabaseResponse databaseSchema = dynamicDataService
                 .getDatabaseSchema(request.schemaName());
@@ -38,12 +44,14 @@ public class TalkToYourDataController {
                 request.question());
 
         if (sql.equalsIgnoreCase(I_CANNOT_ANSWER_THIS_QUESTION_WITH_THE_AVAILABLE_DATA)) {
-            return new QueryResponse(request.conversationId(), request.question(), "N/A",
-                    List.of(Map.of("Error", I_CANNOT_ANSWER_THIS_QUESTION_WITH_THE_AVAILABLE_DATA)));
+            return ResponseEntity
+                    .ok(new QueryResponse(UUID.randomUUID(), request.conversationId(), request.question(), "N/A",
+                            List.of(Map.of("Error", I_CANNOT_ANSWER_THIS_QUESTION_WITH_THE_AVAILABLE_DATA))));
         }
 
         List<Map<String, Object>> result = dynamicDataService.executeQuery(request.schemaName(), sql);
 
-        return new QueryResponse(request.conversationId(), request.question(), sql, result);
+        return ResponseEntity
+                .ok(new QueryResponse(UUID.randomUUID(), request.conversationId(), request.question(), sql, result));
     }
 }
--- src/main/java/com/example/ai/synthetic_data_generator_ai/dto/QueryResponse.java
+++ src/main/java/com/example/ai/synthetic_data_generator_ai/dto/QueryResponse.java
@@ -2,13 +2,15 @@ package com.example.ai.synthetic_data_generator_ai.dto;
 
 import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 
-import com.google.auto.value.AutoValue.Builder;
+import lombok.Builder;
 
 @Builder
 public record QueryResponse(
-    String conversationId,
-    String question,
-    String sqlQuery,
-    List<Map<String, Object>> result) {
+        UUID id,
+        String conversationId,
+        String question,
+        String sqlQuery,
+        List<Map<String, Object>> result) {
 }
